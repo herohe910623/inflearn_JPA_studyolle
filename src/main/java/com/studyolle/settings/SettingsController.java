0bbb4ec6c61ea55bed052846933bf9ecefd0bf4f
herohe910623 <herohe910623@gmail.com>
2023-03-07 19:37:06 +0900

    40.Tag Suggestions List Manually,ObjectMapper

--- src/main/java/com/studyolle/settings/SettingsController.java
+++ src/main/java/com/studyolle/settings/SettingsController.java
@@ -1,5 +1,7 @@
 package com.studyolle.settings;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.studyolle.account.AccountService;
 import com.studyolle.account.CurrentUser;
 import com.studyolle.domain.Account;
@@ -19,6 +21,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -57,6 +60,8 @@ public class SettingsController {
     private final NicknameValidator nicknameValidator;
 
     private final TagRepository tagRepository;
+
+    private final ObjectMapper objectMapper;
 
     @GetMapping(SETTINGS_PROFILE_URL)
     public String updateProfileForm(@CurrentUser Account account, Model model) {
@@ -135,12 +140,15 @@ public class SettingsController {
     }
 
     @GetMapping(SETTINGS_TAGS_URL)
-    public String updateTagsForm(@CurrentUser Account account, Model model) {
+    public String updateTagsForm(@CurrentUser Account account, Model model) throws JsonProcessingException {
         model.addAttribute(account);
         Set<Tag> tags = accountService.getTags(account);
         // 문자열 타입의 리스트로 담을 예정
         model.addAttribute("tags",tags.stream().map(Tag::getTitle).collect(Collectors.toList()));
 
+        // DB 에 있는 모든 태그들 리스트로 나열 자동완성 위해서
+        List<String> allTags = tagRepository.findAll().stream().map(Tag::getTitle).collect(Collectors.toList());
+        model.addAttribute("whitelist",objectMapper.writeValueAsString(allTags));
         return SETTINGS_TAGS_VIEW_NAME;
     }
 
