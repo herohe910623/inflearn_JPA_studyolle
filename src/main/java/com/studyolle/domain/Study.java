92c994ded313633ce14cd64402f386bc409339e9
herohe910623 <herohe910623@gmail.com>
2023-03-20 20:10:50 +0900

    51.new-study,summernote font and review

--- src/main/java/com/studyolle/domain/Study.java
+++ src/main/java/com/studyolle/domain/Study.java
@@ -4,6 +4,7 @@ import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -14,9 +15,9 @@ public class Study {
     @Id @GeneratedValue
     private Long id;
     @ManyToMany
-    private Set<Account> managers;
+    private Set<Account> managers = new HashSet<>();
     @ManyToMany
-    private Set<Account> members;
+    private Set<Account> members = new HashSet<>();
     @Column(unique = true)
     private String path;
     private String title;
@@ -26,9 +27,9 @@ public class Study {
     @Lob @Basic(fetch = FetchType.EAGER)
     private String image;
     @ManyToMany
-    private Set<Tag> tags;
+    private Set<Tag> tags = new HashSet<>();
     @ManyToMany
-    private Set<Zone> zones;
+    private Set<Zone> zones = new HashSet<>();
     private LocalDateTime publishedDateTime;
     private LocalDateTime closedDateTime;
     private LocalDateTime recruitingUpdatedDateTime;
@@ -36,4 +37,8 @@ public class Study {
     private boolean published;
     private boolean closed;
     private boolean useBanner;
+
+    public void addManager(Account account) {
+        this.managers.add(account);
+    }
 }
