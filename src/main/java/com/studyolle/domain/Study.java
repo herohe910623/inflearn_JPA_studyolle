3875b094dcd10483d661d5a609c281993e43ed33
herohe910623 <herohe910623@gmail.com>
2023-03-24 21:26:07 +0900

    55.studySetting banner, cropper Script and image file to consider

--- src/main/java/com/studyolle/domain/Study.java
+++ src/main/java/com/studyolle/domain/Study.java
@@ -58,4 +58,10 @@ public class Study {
     public boolean isManager(UserAccount userAccount) {
         return this.managers.contains(userAccount.getAccount());
     }
+    public void addMember(Account account) {
+        this.members.add(account);
+    }
+    public String getImage() {
+        return image != null ? image : "/images/default_banner.jpeg";
+    }
 }
