b03e74868ab478fe490c04ccc47cb89f7c47f31b
herohe910623 <herohe910623@gmail.com>
2023-05-30 23:07:55 +0900

    73.test db -> postgresql, TestContainers of Docker

--- src/test/java/com/studyolle/infra/MockMvcTest.java
+++ src/test/java/com/studyolle/infra/MockMvcTest.java
@@ -3,6 +3,7 @@ package com.studyolle.infra;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.lang.annotation.ElementType;
@@ -12,10 +13,10 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
+@ActiveProfiles("test")
 @Transactional
 @SpringBootTest
 @AutoConfigureMockMvc
 public @interface MockMvcTest {
-
 
 }
--- src/test/java/com/studyolle/modules/account/AccountControllerTest.java
+++ src/test/java/com/studyolle/modules/account/AccountControllerTest.java
@@ -1,5 +1,6 @@
 package com.studyolle.modules.account;
 
+import com.studyolle.infra.AbstractContainerBaseTest;
 import com.studyolle.infra.MockMvcTest;
 import com.studyolle.infra.mail.EmailMessage;
 import com.studyolle.infra.mail.EmailService;
@@ -26,7 +27,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @MockMvcTest
-public class AccountControllerTest {
+public class AccountControllerTest extends AbstractContainerBaseTest {
 
     @Autowired
     private MockMvc mockMvc;
