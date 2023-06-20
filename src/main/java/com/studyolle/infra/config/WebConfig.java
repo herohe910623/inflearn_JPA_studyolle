ec3b520fe8cbf2d518252c3aa68c8982eda07ed1
herohe910623 <herohe910623@gmail.com>
2023-06-20 22:16:25 +0900

    78.notification icon change,Spring Web MVC HandlerInterceptor

---
+++ src/main/java/com/studyolle/infra/config/WebConfig.java
@@ -0,0 +1,34 @@
+package com.studyolle.infra.config;
+
+import com.studyolle.modules.notification.NotificationInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.StaticResourceLocation;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final NotificationInterceptor notificationInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        List<String> staticResourcesPath = Arrays.stream(StaticResourceLocation.values())
+                .flatMap(StaticResourceLocation::getPatterns).collect(Collectors.toList());
+        staticResourcesPath.add("/node_modules/**");
+        staticResourcesPath.add("/node/**");
+
+        //static 리소스 요청에만 적용하자.
+        registry.addInterceptor(notificationInterceptor)
+                .excludePathPatterns(staticResourcesPath);
+    }
+}
